553c7119fb79c7df73b4216fa66f9db1616d4e4d
sajicode <sajioloye@gmail.com>
2020-02-02 02:08:51 +0100

    setup app boilerplate

---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text, TextInput, StyleSheet } from 'react-native';
+
+const SearchScreen = () => {
+	return (
+		<View>
+			<Text>Search Screen</Text>
+		</View>
+	);
+};
+
+const styles = StyleSheet.create({});
+
+export default SearchScreen;
